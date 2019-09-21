1cdec741421584c15366f610b355579cedddd21b
Douglas Chan <douglas@mediushealth.org>
2019-09-21 15:17:46 +1000

    orbital period update with graphics

--- hashTable.js
+++ hashTable.js
@@ -14,8 +14,51 @@ const hash = (string, maxLimit) => {
   return hash % maxLimit;
 };
 
-class HashTable {
-  constructor() {
-    this.storage = storage;
-  }
-}
+const HashTable = function() {
+  let storage = [];
+  const storageLimit = 4;
+
+  this.print = function() {
+    console.log(storage);
+  };
+
+  this.add = function() {
+    let index = hash(key, value);
+    if (storage[index] === undefined) {
+      storage[index] = [[key, value]];
+    } else {
+      let inserted = false;
+      for (let i = 0; i < storage[index].length; i++) {
+        if (storage[index][i][0] === key) {
+          storage[index][i][1] = value;
+          inserted = true;
+        }
+      }
+      if (inserted === false) {
+        storage[index].push([key, value]);
+      }
+    }
+  };
+
+  this.remove = function(key) {
+    let index = has(key, storageLimit);
+    if (storage[index].length === 1 && storage[index][0][0] === key) {
+      delete storage[index];
+    }
+  };
+
+  this.lookup = function(key) {
+    let index = hash(key, storageLimit)
+    if(storage[index] === undefined) {
+      return undefined
+    } else {
+      for(let i = 0; i<storage[index].length; i++) {
+        if(storage[index][i][0] === key) {
+          return storage[index][i][1]
+        }
+      }
+    }
+};
+
+
+console.log(hash('Doug', 10))
