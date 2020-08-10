0ecfcd8a824c92e188722513540eac14e66f263e
DouggyC <33332978+DouggyC@users.noreply.github.com>
2020-08-10 11:56:14 +1000

    Update destructuring.js

--- ES6_Destructuring/destructuring.js
+++ ES6_Destructuring/destructuring.js
@@ -12,9 +12,10 @@ console.log(rest); // [300 , 400, 500]
 console.log(a, b, rest); // 100 200 { c: 300, d: 400, e: 500 }
 
 //-- Array Destructuring
-// const people = ['John', 'Beth', 'Mike'];
-// const [person1, person2, person3] = people;
-// console.log(person1, person2, person3); // John Beth Mike
+const people = ['John', 'Beth', 'Mike'];
+const [person1, person2, person3] = people;
+console.log(person1, person2, person3); // John Beth Mike
+
 
 //-- Parse array returned from function
 function getPeople() {
@@ -23,6 +24,7 @@ function getPeople() {
 let person1, person2, person3;
 [person1, person2, person3] = getPeople();
 console.log(person1, person2, person3); // John Beth Mike
+
 
 //-- Object Destructuring
 const person = {
@@ -45,9 +47,16 @@ const { name, age, city, sayHello } = person;
 console.log(name, age, city); // Hoe John 32 Miami
 sayHello(); // Hello
 
+
 // ES6+ method
 // Since ES6, you can also swap variables more elegantly. You can use destructuring assignment array matching. It’s simply.
 
 [a, b] = [b, a];
 
+// Array matching
 let [a, b, c] = [1, 2, 3];
+console.log(a, b, c); // 1 2 3
+
+// object destructure naming with variable alias
+let { id: uuid } = { id: 12345678 }
+console.log(uuid); // 12345678
